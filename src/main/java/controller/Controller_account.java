16e11a6db72598baf10008093e135ec43cf36363
ilfeza <120182551+ilfeza@users.noreply.github.com>
2023-07-02 19:58:38 +0300

    добавление основной сцены аккаунта

--- src/main/java/controller/Controller_account.java
+++ src/main/java/controller/Controller_account.java
@@ -2,6 +2,9 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import model.Model;
 
@@ -10,16 +13,30 @@ import java.sql.SQLException;
 public class Controller_account  implements EventHandler<ActionEvent> {
     private Stage stage;
     private Model model;
+    private Button change_data = new Button("Изменить рег. данные");
+    private Group root = new Group();
+    private Scene scene = new Scene(root, 720, 480);
+
 
     public Controller_account(Model model, Stage stage) throws SQLException {
         this.model = model;
         this.stage = stage;
 
+        change_data.setLayoutX(360);
+        change_data.setLayoutY(140);
+        change_data.addEventHandler(ActionEvent.ACTION, this);
+        root.getChildren().add(change_data);
 
+        stage.setScene(scene);
+        stage.show();
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
+        if(actionEvent.getSource() == change_data){
+
+
+        }
 
     }
 }
